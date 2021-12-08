9d8bea3cb86ba07387e1d65ec811d0f2c3b627d3
nikogianna <giannakis.n@pg.com>
2021-12-08 14:26:44 +0100

    third commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + Niko!";
+		return "Greetingss from Spring Boot + Tanzu + Niko!";
 	}
 
 }
