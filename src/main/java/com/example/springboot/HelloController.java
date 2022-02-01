e95927f257ef6127c44067cbf78b2cf4ec8b1647
nikogianna <nikogianna19@gmail.com>
2022-02-01 12:32:21 +0100

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + Niko! (Now with TLS";
+		return "Greetings from Spring Boot + Tanzu + Niko! (Now with TLS)";
 	}
 
 }
